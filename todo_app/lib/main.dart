9333f7a76a02770360c82aa0a67c72c4dad14172
x1jy0 <x1jy0worker@gmail.com>
2024-07-02 16:16:16 +0900

    仮で主なフレームを作成

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,125 +1,267 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TodoListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TodoListScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TodoListScreenState createState() => _TodoListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<Task> tasks = [];
+  List<Task> archivedTasks = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _loadTasks();
+  }
+
+  _loadTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> taskStrings = prefs.getStringList('tasks') ?? [];
+    List<String> archivedTaskStrings =
+        prefs.getStringList('archivedTasks') ?? [];
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      tasks = taskStrings.map((task) => Task.fromJson(task)).toList();
+      archivedTasks =
+          archivedTaskStrings.map((task) => Task.fromJson(task)).toList();
+    });
+  }
+
+  _saveTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> taskStrings = tasks.map((task) => task.toJson()).toList();
+    List<String> archivedTaskStrings =
+        archivedTasks.map((task) => task.toJson()).toList();
+
+    await prefs.setStringList('tasks', taskStrings);
+    await prefs.setStringList('archivedTasks', archivedTaskStrings);
+  }
+
+  void _addTask(Task task) {
+    setState(() {
+      tasks.add(task);
+    });
+    _saveTasks();
+  }
+
+  void _archiveTask(Task task) {
+    setState(() {
+      tasks.remove(task);
+      archivedTasks.add(task);
+    });
+    _saveTasks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return TaskWidget(
+                  task: tasks[index],
+                  onArchive: () => _archiveTask(tasks[index]),
+                );
+              },
+            ),
+          ),
+          Divider(),
+          Text('Archived Tasks'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: archivedTasks.length,
+              itemBuilder: (context, index) {
+                return TaskWidget(
+                  task: archivedTasks[index],
+                  isArchived: true,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return NewTaskDialog(
+                onAddTask: _addTask,
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Task {
+  String title;
+  List<Task> subTasks;
+  bool isCompleted;
+
+  Task(
+      {required this.title,
+      this.subTasks = const [],
+      this.isCompleted = false});
+
+  Task.fromJson(String json)
+      : title = json.split('|')[0],
+        subTasks = json.split('|').length > 1
+            ? (json
+                .split('|')[1]
+                .split(';')
+                .map((subTask) => Task.fromJson(subTask))
+                .toList())
+            : [],
+        isCompleted =
+            json.split('|').length > 2 ? json.split('|')[2] == 'true' : false;
+
+  String toJson() {
+    return '$title|${subTasks.map((subTask) => subTask.toJson()).join(';')}|$isCompleted';
+  }
+}
+
+class TaskWidget extends StatefulWidget {
+  final Task task;
+  final bool isArchived;
+  final VoidCallback? onArchive;
+
+  TaskWidget({required this.task, this.isArchived = false, this.onArchive});
+
+  @override
+  _TaskWidgetState createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.task.title),
+      leading: Checkbox(
+        value: widget.task.isCompleted,
+        onChanged: (bool? value) {
+          setState(() {
+            widget.task.isCompleted = value!;
+          });
+        },
+      ),
+      trailing: widget.isArchived
+          ? null
+          : IconButton(
+              icon: Icon(Icons.archive),
+              onPressed: widget.onArchive,
+            ),
+      onTap: () {
+        setState(() {
+          _isExpanded = !_isExpanded;
+        });
+      },
+      subtitle: _isExpanded
+          ? Column(
+              children: widget.task.subTasks
+                  .map((subTask) => ListTile(
+                        title: Text(subTask.title),
+                        leading: Checkbox(
+                          value: subTask.isCompleted,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              subTask.isCompleted = value!;
+                            });
+                          },
+                        ),
+                      ))
+                  .toList(),
+            )
+          : null,
+    );
+  }
+}
+
+class NewTaskDialog extends StatefulWidget {
+  final Function(Task) onAddTask;
+
+  NewTaskDialog({required this.onAddTask});
+
+  @override
+  _NewTaskDialogState createState() => _NewTaskDialogState();
+}
+
+class _NewTaskDialogState extends State<NewTaskDialog> {
+  TextEditingController _titleController = TextEditingController();
+  List<Task> _subTasks = [];
+
+  void _addSubTask(String title) {
+    setState(() {
+      _subTasks.add(Task(title: title));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return AlertDialog(
+      title: Text('New Task'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: _titleController,
+            decoration: InputDecoration(labelText: 'Task Title'),
+          ),
+          ..._subTasks
+              .map((subTask) => ListTile(title: Text(subTask.title)))
+              .toList(),
+          TextField(
+            decoration: InputDecoration(labelText: 'Sub Task Title'),
+            onSubmitted: _addSubTask,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        TextButton(
+          onPressed: () {
+            widget.onAddTask(
+                Task(title: _titleController.text, subTasks: _subTasks));
+            Navigator.of(context).pop();
+          },
+          child: Text('Add'),
+        ),
+      ],
     );
   }
 }
